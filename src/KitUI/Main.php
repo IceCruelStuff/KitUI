cb26179acb6c40a8bef8d76b1fe70119be67d350
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-04-14 19:48:05 -0700

    Update Main.php

--- src/KitUI/Main.php
+++ src/KitUI/Main.php
@@ -7,8 +7,14 @@ use jojoe77777\FormAPI\SimpleForm;
 
 class Main extends PluginBase {
 
+    public $kits = [];
+
     public function onEnable() {
         $this->saveDefaultConfig();
+        $kits = $this->getConfig()->get("kits");
+        foreach ($kits as $key => $value) {
+            $this->kits[$key] = $value;
+        }
     }
 
 }
