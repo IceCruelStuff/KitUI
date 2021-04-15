15cfc52a0c8a65659e7a409d659341bc5c22bf06
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-04-14 19:37:29 -0700

    Create Main.php

---
+++ src/KitUI/Main.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace KitUI;
+
+use pocketmine\plugin\PluginBase;
+use jojoe77777\FormAPI\SimpleForm;
+
+class Main extends PluginBase {
+
+    public function onEnable() {
+        $this->saveDefaultConfig();
+    }
+
+}
