25897c1ee3cb841fdbab8ea37f92117ea8847a67
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-04-27 19:08:03 -0700

    Update KitsCommand.php

--- src/KitUI/Commands/KitsCommand.php
+++ src/KitUI/Commands/KitsCommand.php
@@ -11,6 +11,7 @@ use pocketmine\item\Item;
 use pocketmine\plugin\Plugin;
 use pocketmine\utils\TextFormat;
 use pocketmine\Player;
+use jojoe77777\FormAPI\SimpleForm;
 use KitUI\Main;
 
 class KitsCommand extends Command implements PluginIdentifiableCommand {
@@ -40,29 +41,63 @@ class KitsCommand extends Command implements PluginIdentifiableCommand {
             if (isset($args[0])) {
                 if (array_key_exists($args[0], $this->plugin->kits)) {
                     $kit = $this->plugin->kits[$args[0]];
-                    foreach ($kit as $kitItem) {
-                        $amount = 1;
-                        if (array_key_exists("amount", $kitItem)) {
-                            $amount = (int) $kitItem["amount"];
-                        }
-                        $item = Item::get($kitItem["id"], 0, $amount);
-                        if (isset($kitItem["enchantments"])) {
-                            foreach ($kitItem["enchantments"] as $key => $value) {
-                                $item->addEnchantment(new EnchantmentInstance(Enchantment::getEnchantmentByName($key), $value));
-                            }
-                        }
-                        $sender->getInventory()->addItem($item);
-                    }
+                    $this->giveKit($sender, $kit);
                     $sender->sendMessage(TextFormat::GREEN . "You received " . $args[0] . " kit");
                 } else {
                     $sender->sendMessage(TextFormat::RED . "That kit doesn't exist");
                 }
             } else {
-                $sender->sendMessage(TextFormat::RED . "Please specify a kit");
+                $this->sendForm($sender);
             }
         } else {
             $sender->sendMessage(TextFormat::RED . "Please run this command in-game");
         }
+    }
+
+    private function giveKit($player, $kit) {
+        foreach ($kit as $kitItem) {
+            $amount = 1;
+            if (array_key_exists("amount", $kitItem)) {
+                $amount = (int) $kitItem["amount"];
+            }
+            $item = Item::get($kitItem["id"], 0, $amount);
+            if (isset($kitItem["enchantments"])) {
+                foreach ($kitItem["enchantments"] as $key => $value) {
+                    $item->addEnchantment(new EnchantmentInstance(Enchantment::getEnchantmentByName($key), $value));
+                }
+            }
+            $sender->getInventory()->addItem($item);
+        }
+    }
+
+    private function sendForm($sender) {
+        $form = new SimpleForm(function (Player $player, array $data = null) {
+            if ($data === null) {
+                return;
+            }
+
+            $kits = [];
+            foreach ($this->plugin->kits as $kit) {
+                $kits[] = $kit;
+            }
+            $closeButtonIndex = count($kits) - 1;
+            for ($i = 0; $i < $closeButtonIndex; $i++) {
+                switch ($data) {
+                    case $i:
+                        $this->giveKit($player, $kits[$i]);
+                        break;
+                }
+            }
+            switch ($data) {
+                case $closeButtonIndex:
+                    break;
+            }
+        });
+        $form->setTitle("Kits");
+        foreach ($this->plugin->kits as $key => $value) {
+            $form->addButton($key);
+        }
+        $form->addButton("Close");
     }
 
 }
