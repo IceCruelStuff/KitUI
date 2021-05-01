0f48a1e32a6fe616d3e30f9d0c67614a0d5904c5
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-05-01 09:52:04 -0700

    [ci skip]

--- src/KitUI/KitUI.php
+++ src/KitUI/KitUI.php
@@ -7,7 +7,7 @@ use pocketmine\plugin\PluginBase;
 use jojoe77777\FormAPI\SimpleForm;
 use KitUI\Commands\KitsCommand;
 
-class Main extends PluginBase {
+class KitUI extends PluginBase {
 
     public $kits = [];
 
